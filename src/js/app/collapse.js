d3ee1611025ae0e009f3e44f5469b29052ee29ee
ru39391 <dev@39391.ru>
2022-02-28 02:14:35 +0300

    personal, pt. 2

--- src/js/app/collapse.js
+++ src/js/app/collapse.js
@@ -11,3 +11,9 @@ document.querySelectorAll('.table-row__body .readmore').forEach(tableRowToggler 
     e.target.closest('.table-row').classList.remove('active');
   })
 });
+
+document.body.addEventListener('click', e => {
+  if (e.target.closest('.section__expander')) {
+    e.target.closest('.section__expander').classList.toggle('active');
+  }
+});
