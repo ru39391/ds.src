1c8137b57d113605c1290ff9bfb01fe3b975e822
ru39391 <dev@39391.ru>
2022-05-09 02:49:45 +0300

    feature/datepicker, final

--- src/js/app/datepicker.js
+++ src/js/app/datepicker.js
@@ -24,13 +24,65 @@ const datepicker = new Datepicker(bookingFormDatepicker, {
   minDate: 'today'
 });
 
+const bookingForm = document.querySelector('.booking-form');
 const bookingDate = document.querySelector('.booking-heading__date');
+const bookingTime = document.querySelector('.booking-heading__time');
+const bookingHeadings = document.querySelectorAll('.booking-heading');
 const datepickerCells = document.querySelectorAll('.datepicker-cell');
+const bookingFormLabels = document.querySelectorAll('.booking-form__label');
+const bookingFormCols = Array.from(document.querySelectorAll('.booking-form__col'));
+
+function removeInactiveClass(elem, selector) {
+  if(elem.classList.contains(selector)) {
+    elem.classList.remove(selector);
+  }
+};
+function addInactiveClass(elem, selector) {
+  if(!elem.classList.contains(selector)) {
+    elem.classList.add(selector);
+  }
+};
+function removeHeadingClass(target, elem, selector) {
+  if(target != elem.getAttribute('data-target')) {
+    removeInactiveClass(elem, selector);
+  }
+};
+
 if(bookingDate) {
   datepickerCells.forEach(datepickerCellsEl => {
     datepickerCellsEl.addEventListener('click', e => {
+      const currentColID = e.target.closest('.booking-form__col').id;
       const pickedDate = new Date(Number(e.target.getAttribute('data-date')));
       bookingDate.textContent = `${pickedDate.getDate()} ${Datepicker.locales.ru.monthsShort[pickedDate.getMonth()]}`;
+      bookingTime.textContent = '';
+      bookingFormLabels.forEach(bookingFormLabelsEl => {
+        removeInactiveClass(bookingFormLabelsEl, 'booking-form__label_inactive');
+      });
+      bookingHeadings.forEach(bookingHeadingsEl => {
+        removeHeadingClass(currentColID, bookingHeadingsEl, 'booking-heading_active');
+      });
     });
   });
 };
+
+bookingFormLabels.forEach(bookingFormLabelsEl => {
+  bookingFormLabelsEl.addEventListener('click', e => {
+    bookingTime.textContent = `| ${e.target.textContent}`;
+    bookingHeadings.forEach(bookingHeadingsEl => {
+      addInactiveClass(bookingHeadingsEl, 'booking-heading_active');
+    });
+  });
+});
+
+bookingHeadings.forEach(bookingHeadingsEl => {
+  bookingHeadingsEl.addEventListener('click', e => {
+    const bookingHeadingTarget = e.currentTarget.getAttribute('data-target');
+    const bookingFormColCurr = bookingFormCols.indexOf(document.querySelector(`#${bookingHeadingTarget}`));
+    bookingForm.style.transform = `translateX(-${100*bookingFormColCurr}%)`;
+    if(bookingFormColCurr == bookingFormCols.length - 1){
+      addInactiveClass(e.currentTarget.closest('.row').querySelector('.booking-heading'), 'booking-heading_checked');
+    } else {
+      removeInactiveClass(e.currentTarget.closest('.row').querySelector('.booking-heading'), 'booking-heading_checked');
+    };
+  });
+});
