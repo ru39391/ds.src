99d79e56551d3481d65148eecd2b2bcc5fcc6d48
ru39391 <dev@39391.ru>
2022-05-21 00:47:34 +0300

    review/header

--- src/js/app/header.js
+++ src/js/app/header.js
@@ -1,7 +1,25 @@
-window.addEventListener('scroll', () => {
-  if(window.scrollY > document.querySelector('.header').scrollHeight) {
-      document.querySelector('.header').classList.add('header_sticky');
-  } else {
-      document.querySelector('.header').classList.remove('header_sticky');
+const changeHeader = (() => {
+  const header = document.querySelector('.header');
+  const scrollHeight = 200;
+  let scrolled = false;
+
+  function init() {
+    window.addEventListener('scroll', e => {
+      if(!scrolled) {
+        scrolled = true;
+        setTimeout(scrollPage, 250);
+      }
+    }, false);
   }
-});
+
+  function scrollPage() {
+    if (window.pageYOffset >= scrollHeight) {
+      header.classList.add('header_sticky');
+    } else {
+      header.classList.remove('header_sticky');
+    }
+    scrolled = false;
+  }
+
+  init();
+})();
