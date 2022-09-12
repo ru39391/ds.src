3bb1e900e106a01234955ed96edcacc5eaa8b8e0
ru39391 <dev@39391.ru>
2022-09-12 15:40:06 +0300

    support/обновить верхнюю панель шапки

--- src/js/app/menu.js
+++ src/js/app/menu.js
@@ -1,26 +1,28 @@
-const headerTop = document.querySelector('.header_top');
-const menuTitle = document.querySelector('.menu__title');
+const topPanel = document.querySelector('.top-panel');
+const menuSel = '.navbar';
+const menuTitle = document.querySelector('.navbar__title');
+const menuTitleActiveClass = 'navbar__title_active';
 
 function toggleMenu(togglerEl,activeClass) {
   if(togglerEl) {
     togglerEl.classList.toggle(activeClass);
-    headerTop.style.zIndex = 1040;
+    topPanel.style.zIndex = 1040;
   }
 };
 
 function hideMenu(togglerEl,activeClass) {
   if(togglerEl) {
     togglerEl.classList.remove(activeClass);
-    headerTop.style = null;
+    topPanel.style = null;
   }
 };
 
 menuTitle.addEventListener('click', e => {
-  toggleMenu(menuTitle, 'menu__title_active');
+  toggleMenu(menuTitle, menuTitleActiveClass);
 });
 
 document.body.addEventListener('click', e => {
-  if(!e.target.closest('.menu') && menuTitle.classList.contains('menu__title_active')) {
-    hideMenu(menuTitle, 'menu__title_active');
+  if(!e.target.closest(menuSel) && menuTitle.classList.contains(menuTitleActiveClass)) {
+    hideMenu(menuTitle, menuTitleActiveClass);
   }
 });
