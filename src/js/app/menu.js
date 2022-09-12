7927f4d63cde0db384b280e283f1cdb5ceade733
ru39391 <dev@39391.ru>
2022-09-13 02:03:20 +0300

    support/обновить вёрстку нижнего блока шапки

--- src/js/app/menu.js
+++ src/js/app/menu.js
@@ -3,17 +3,25 @@ const menuSel = '.navbar';
 const menuTitle = document.querySelector('.navbar__title');
 const menuTitleActiveClass = 'navbar__title_active';
 
-function toggleMenu(togglerEl,activeClass) {
+function hideMenu(togglerEl, activeClass) {
   if(togglerEl) {
-    togglerEl.classList.toggle(activeClass);
+    togglerEl.classList.remove(activeClass);
+    topPanel.style = null;
+  }
+};
+
+function showMenu(togglerEl, activeClass) {
+  if(togglerEl) {
+    togglerEl.classList.add(activeClass);
     topPanel.style.zIndex = 1040;
   }
 };
 
-function hideMenu(togglerEl,activeClass) {
-  if(togglerEl) {
-    togglerEl.classList.remove(activeClass);
-    topPanel.style = null;
+function toggleMenu(togglerEl, activeClass) {
+  if(togglerEl.classList.contains(activeClass)) {
+    hideMenu(togglerEl, activeClass);
+  } else {
+    showMenu(togglerEl, activeClass);
   }
 };
 
@@ -22,6 +30,7 @@ menuTitle.addEventListener('click', e => {
 });
 
 document.body.addEventListener('click', e => {
+  console.log(!e.target.closest(menuSel), menuTitle.classList.contains(menuTitleActiveClass));
   if(!e.target.closest(menuSel) && menuTitle.classList.contains(menuTitleActiveClass)) {
     hideMenu(menuTitle, menuTitleActiveClass);
   }
